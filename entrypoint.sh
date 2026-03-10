5b623f36f027ef21d4b4c763897e43692eeac558
Eros Deva <erosdeva@mac.lan>
2026-03-10 17:41:56 -0500

    Move the Dockerfile in the preceding state.

--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Start the first process and put it in the background
-python agent.py &
-
-# Start the second process
-streamlit run dashboard.py --server.port=8080 --server.address=0.0.0.0
